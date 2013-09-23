79e9530375bd4a3df203d5d057bbf30ec0e3ab01
Mher Sarkissian <chating@gmail.com>
2013-09-22 18:17:01 -0700

    initial checkin

---
+++ armenian news/NewsApp/NewsApp/Controllers/NewsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewsApp.Models;
+using PagedList;
+using System.Data.Objects;
+
+namespace NewsApp.Controllers
+{ 
+    public class NewsController : Controller
+    {
+        private HilurEntities db = new HilurEntities();
+
+        //
+        // GET: /NewNews/
+
+        public ViewResult Index(int? page)
+        {
+            int pageSize = 30;
+            int pageIndex = (page ?? 1);
+
+            ViewBag.Source = "Index";
+
+            return View(db.Entries.OrderByDescending(x => x.PubDate).ToPagedList(pageIndex,pageSize));
+        }
+
+        public ActionResult Popular(int? page)
+        {
+            int pageSize = 30;
+            int pageIndex = (page ?? 1);
+
+            var scoredResult = db.Entries.Select(o => new { calcScore = (o.Score * 2) - (EntityFunctions.DiffDays(o.PubDate, DateTime.Now) * 2), o });
+            var query = scoredResult.OrderByDescending(x => x.calcScore).Select(a => a.o).ToPagedList(pageIndex, pageSize);
+
+            ViewBag.Source = "Popular";
+
+            return View("Index",query);
+        }
+
+        //
+        // GET: /NewNews/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Entry entry = db.Entries.Include("Comments").Single(a => a.Id == id);
+            return View(entry);
+        }
+
+        [HttpPost]
+        public ActionResult Details(int id, string comment)
+        {
+            Entry article = db.Entries.Include("Comments").Single(a => a.Id == id);
+
+            if (Request.IsAuthenticated)
+            {
+                SiteUser user = db.SiteUsers.Single(n => n.Username == User.Identity.Name);
+
+                Comment commentEntity = new Comment();
+                commentEntity.ArticleId = article.Id;
+                commentEntity.Description = comment;
+                commentEntity.Title = "TBD";
+                commentEntity.Date = DateTime.Now;
+                commentEntity.UserId = user.Id;
+
+                user.PostCount++;
+                article.Comments.Add(commentEntity);
+                db.SaveChanges();
+            }
+
+            return View(article);
+        }
+
+        [HttpPost]
+        public bool VoteUp(int id)
+        {
+            bool result = false;
+            if (Request.IsAuthenticated)
+            {
+                SiteUser user = db.SiteUsers.Single(n => n.Username == User.Identity.Name);
+
+                var votequery = db.Votes.Where(x => x.ArticleId == id && x.UserId == user.Id);
+
+                if (votequery.Count() == 0)
+                {
+                    Vote newScore = new Vote();
+                    newScore.UserId = user.Id;
+                    newScore.ArticleId = id;
+                    newScore.Date = DateTime.Now;
+                    newScore.Score = true;
+                    db.Votes.Add(newScore);
+
+                    var article = db.Entries.Single(a => a.Id == id);
+                    article.Score++;
+
+                    db.SaveChanges();
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        [HttpPost]
+        public bool VoteDown(int id)
+        {
+
+            bool result = false;
+            if (Request.IsAuthenticated)
+            {
+                SiteUser user = db.SiteUsers.Single(n => n.Username == User.Identity.Name);
+
+                var votequery = db.Votes.Where(x => x.ArticleId == id && x.UserId == user.Id);
+
+                if (votequery.Count() == 0)
+                {
+                    Vote newScore = new Vote();
+                    newScore.UserId = user.Id;
+                    newScore.ArticleId = id;
+                    newScore.Date = DateTime.Now;
+                    newScore.Score = false;
+                    db.Votes.Add(newScore);
+
+                    var article = db.Entries.Single(a => a.Id == id);
+                    article.Score--;
+
+                    db.SaveChanges();
+                    result = true;
+                }
+            }
+            return result;
+        }
+                     
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ armenian news/NewsApp/NewsApp/Global.asax.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Data.SqlClient;
+using System.Data;
+using System.Net;
+using System.Configuration;
+using System.Web.Configuration;
+using System.IO;
+
+namespace NewsApp
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+            //load new content if not done since one day
+            DateTime lastLoad = DateTime.Parse(ConfigurationManager.AppSettings["LastLoad"]);
+
+            if ((DateTime.Now - lastLoad).Days >= 1)
+            {
+                SaveEntries(GetEntries());
+
+                var config = WebConfigurationManager.OpenWebConfiguration("~");
+                config.AppSettings.Settings["LastLoad"].Value = DateTime.Now.ToShortDateString();
+                config.Save(ConfigurationSaveMode.Minimal, false);
+            }
+
+            
+        }
+
+        private List<RssFeedItem> GetEntries()
+        {
+            //create a new list of the rss feed items to return
+            List<RssFeedItem> rssFeedItems = new List<RssFeedItem>();
+
+            //create an http request which will be used to retrieve the rss feed
+            HttpWebRequest rssFeed = (HttpWebRequest)WebRequest.Create("http://xecutrix.usc.edu/news/index.rss");
+
+            //use a dataset to retrieve the rss feed
+            using (DataSet rssData = new DataSet())
+            {
+                //read the xml from the stream of the web request
+                rssData.ReadXml(rssFeed.GetResponse().GetResponseStream());
+
+                //loop through the rss items in the dataset and populate the list of rss feed items
+                foreach (DataRow dataRow in rssData.Tables["item"].Rows)
+                {
+                    string dateString = Convert.ToString(dataRow["pubDate"]).Replace(" (PDT)", "").Replace(" (PST)", "").Replace(",", "");
+                    DateTime parsedDate = DateTime.ParseExact(dateString, "ddd d MMM yyyy HH:mm:ss zzz", null);
+                    string link = Convert.ToString(dataRow["link"]);
+                    string title = Convert.ToString(dataRow["title"]);
+                    //fetch content
+                    string post = FetchPost(link);
+
+                    rssFeedItems.Add(new RssFeedItem
+                    {
+                        Link = link,
+                        PublishDate = parsedDate,
+                        Title = title,
+                        Post = post
+                    });
+                }
+            }
+
+            return rssFeedItems;
+        }
+
+        private string FetchPost(string link)
+        {
+            WebRequest request = WebRequest.Create(link);
+            WebResponse response = request.GetResponse();
+            Stream data = response.GetResponseStream();
+            string html = String.Empty;
+            using (StreamReader sr = new StreamReader(data))
+            {
+                html = sr.ReadToEnd();
+            }
+            int startIndex = html.IndexOf("<pre>") + "<pre>".Length;
+            int endIndex = html.IndexOf("</pre>");
+
+            return html.Substring(startIndex,endIndex - startIndex);
+        }
+
+        public void SaveEntries(List<RssFeedItem> list)
+        {
+            string connection = ConfigurationManager.ConnectionStrings["Hilur"].ToString();
+            using (SqlConnection sqlConn = new SqlConnection(connection))
+            {
+                sqlConn.Open();
+                foreach (var entry in list)
+                {
+                    using (SqlCommand sqlCmd = new SqlCommand())
+                    {
+                        sqlCmd.CommandType = CommandType.StoredProcedure;
+                        sqlCmd.CommandText = "Insert_Entry";
+                        sqlCmd.Connection = sqlConn;
+                        sqlCmd.Parameters.Add(new SqlParameter("@title", entry.Title));
+                        sqlCmd.Parameters.Add(new SqlParameter("@pubDate", entry.PublishDate));
+                        sqlCmd.Parameters.Add(new SqlParameter("@link", entry.Link));
+                        sqlCmd.Parameters.Add(new SqlParameter("@post", entry.Post));
+                        sqlCmd.ExecuteNonQuery();
+                    }
+                }
+                sqlConn.Close();
+            }
+        }
+
+        /// <summary>
+        /// RSS feed item entity
+        /// </summary>
+        public class RssFeedItem
+        {
+            /// <summary>
+            /// Gets or sets the title
+            /// </summary>
+            public string Title { get; set; }
+
+            /// <summary>
+            /// Gets or sets the link
+            /// </summary>
+            public string Link { get; set; }
+
+            /// <summary>
+            /// Gets or sets the post
+            /// </summary>
+            public string Post { get; set; }
+
+            /// <summary>
+            /// Gets or sets the publish date
+            /// </summary>
+            public DateTime PublishDate { get; set; }
+
+        }
+    }
+}
